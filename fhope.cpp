5b95cb704a24d0ca09dda9a4486e9459d618c9a9
ns <notanemail@notanemail.notanemail>
2022-05-21 11:44:43 +0800

    Rewrite enc and dec

--- fhope.cpp
+++ fhope.cpp
@@ -4,11 +4,18 @@
 #include <sstream>
 
 int cnt = 0;
+static string unicodes[] = {"\u2080", "\u2081", "\u2082", "\u2083", "\u2084", "\u2085", "\u2086", "\u2087", "\u2088", "\u2089"};
 ct_t rndEnc(int pt) {
-    return to_string(pt) + "c" + to_string(cnt++);
+    string rndSubNum = to_string(cnt++);
+    stringstream ss;
+    ss << to_string(pt) << "\u2091";
+    for(char& c: rndSubNum) {
+        ss << unicodes[c - '0'];
+    }
+    return ss.str();
 }
 int rndDec(ct_t ct) {
-    return stoi(ct.substr(0, ct.find('c')));
+    return stoi(ct.substr(0, ct.find("\u2091")));
 }
 ct_t fhope::enc(int pt) {
     ct_t ct = rndEnc(pt);
